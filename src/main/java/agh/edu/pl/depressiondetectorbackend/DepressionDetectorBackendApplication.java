e86c11e057cca30fb2eba874c948764c6b50c14a
Radomir Krawczykiewicz <kotwic4@gmail.com>
2019-05-30 14:29:11 +0200

    init commit

---
+++ src/main/java/agh/edu/pl/depressiondetectorbackend/DepressionDetectorBackendApplication.java
@@ -0,0 +1,13 @@
+package agh.edu.pl.depressiondetectorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepressionDetectorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepressionDetectorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agh/edu/pl/depressiondetectorbackend/DepressionDetectorBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package agh.edu.pl.depressiondetectorbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepressionDetectorBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
